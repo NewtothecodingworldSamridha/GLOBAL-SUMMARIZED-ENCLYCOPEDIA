2562b0424ddcefb2b9dd0fa95eeec1c68b6ace0f
Samridha Banerjee <banerjeesamridha7@gmail.com>
2025-10-29 21:22:46 +0530

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+```
+
+**File: `src/App.jsx`**
+Copy the entire React component code from the artifact (the complete KnowledgeApp component)
+
+**File: `.gitignore`**
+```
+node_modules
+dist
+.DS_Store
